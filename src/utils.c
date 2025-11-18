02dbaea7bc3f2d46d68eada7216a113292b805a7
HANIF ALFA RIZKY <hanif.alfa4149@student.unri.ac.id>
2025-11-18 21:40:40 +0700

    Add utility functions for screen management and string manipulation

---
+++ src/utils.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "utils.h"
+
+void clearScreen()
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void pauseScreen()
+{
+    printf("\nTekan ENTER untuk melanjutkan...");
+    getchar();
+    getchar();
+}
+
+void trimTrailingNewline(char *str)
+{
+    int len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n')
+        str[len - 1] = '\0';
+}
